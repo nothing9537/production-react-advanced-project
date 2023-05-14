1e5d13e0d4356007fdb51b58d9dabc7fda3cad46
Nothingg <nothing9537@gmail.com>
2023-05-14 18:19:16 -0500

    14. add Stylelint + i18n plugin for eslint

--- src/widgets/Aside/ui/Aside/Aside.tsx
+++ src/widgets/Aside/ui/Aside/Aside.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable i18next/no-literal-string */
 import { FC, useState } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { LanguageSwitcher } from 'widgets/LanguageSwitcher'
