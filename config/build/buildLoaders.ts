30e5542d1ddc3f121c7c589c5a1275392ef9d1dd
Nothingg <nothing9537@gmail.com>
2023-07-14 10:28:19 -0500

    Update buildLoaders.ts

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,3 +1,4 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { RuleSetRule } from 'webpack'
 import { buildCSSLoader } from './loaders/buildCSSLoader'
 import { buildSVGLoder } from './loaders/buildSVGLoader'
@@ -32,7 +33,24 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
 		}
 	}
 
-	const cssLoader = buildCSSLoader(isDev)
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev
+							? '[path][name]__[local]'
+							: '[hash:base64:8]',
+					},
+				}
+			},
+			'sass-loader',
+		]
+	}
 
 	const typescriptLoader: RuleSetRule = {
 		test: /\.tsx?$/,
