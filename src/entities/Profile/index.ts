5c45b8d168323b8e6966ca8942064421b23583b0
Nothingg <nothing9537@gmail.com>
2023-10-04 13:15:52 -0500

    Update index.ts

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,11 +1,3 @@
 export { Profile, ProfileSchema } from './model/types/profile';
 export { profileActions, profileReducer } from '../../features/EditableProfileCard/model/slice/profileSlice';
-// export { fetchProfileData } from './model/services/fetchProfileData/fetchProfileData';
-// export { updateProfileData } from './model/services/updateProfileData/updateProfileData';
 export { ProfileCard } from './ui/ProfileCard/ProfileCard';
-
-// export { getProfileData } from './model/selectors/getProfileData/getProfileData';
-// export { getProfileError } from './model/selectors/getProfileError/getProfileError';
-// export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getProfileIsLoading';
-// export { getProfileReadonly } from './model/selectors/getProfileReadonly/getProfileReadonly';
-// export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm';
