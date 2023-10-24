197670f5e4e2480531f270fb02bc12f7abad1e77
Nothingg <nothing9537@gmail.com>
2023-10-24 11:16:21 -0500

    lint

--- src/shared/consts/translationNamespaces.ts
+++ src/shared/consts/translationNamespaces.ts
@@ -1,3 +1,3 @@
-import { TranslationNamespacesKeys } from "@/shared/types/translation";
+import { TranslationNamespacesKeys } from '@/shared/types/translation';
 
 export const translationNamespaces: TranslationNamespacesKeys[] = ['articles', 'about', 'aside', 'main', 'navbar', 'profile', 'translation', 'validate'];
--- src/shared/lib/hooks/useAppTranslation.ts
+++ src/shared/lib/hooks/useAppTranslation.ts
@@ -1,4 +1,4 @@
 import { useTranslation } from 'react-i18next';
-import { TranslationNamespacesKeys } from "../../types/translation";
+import { TranslationNamespacesKeys } from '../../types/translation';
 
 export const useAppTranslation = useTranslation<TranslationNamespacesKeys>;
