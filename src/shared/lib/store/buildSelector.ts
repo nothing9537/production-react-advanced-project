6882390fc2bb258675718cd7bfeb3975f0ab89a4
Nothingg <nothing9537@gmail.com>
2023-10-26 09:10:09 -0500

    buildSeelctor types

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends unknown[]> = (state: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends unknown[]> = (...args: Args) => T;
+type Result<T, Args extends unknown[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export const buildSelector = <T>(selector: Selector<T>): Result<T> => {
-  const useSelectorHook = () => {
-    return useSelector(selector);
+export const buildSelector = <T, Args extends unknown[]>(selector: Selector<T, Args>): Result<T, Args> => {
+  const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+    return useSelector((state: StateSchema) => selector(state, ...args));
   };
 
   return [useSelectorHook, selector];
